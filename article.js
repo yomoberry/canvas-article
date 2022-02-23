d6bed3363ede11fa92ad4f2460bf3c00c93ed643
yomoberry <4morgan.9loops.leidbert@gmail.com>
2022-02-24 01:07:06 +0900

    途中

--- article.js
+++ article.js
@@ -1,46 +1,70 @@
-// 改行文字で折り返し
+// 改行文字で折り返し＆段落の先頭にスペース
 function WrapLF(arr){
 	let result_array = [];
 	for(var i = 0; i < arr.length; i++){
 		let splited = arr[i].split("\n");
-		Array.prototype.push.apply(result_array, splited)
+		// 段落の先頭にスペース
+		for(var j = 0; j < splited.length; j++){
+			if(splited[j].length > 0){
+				splited[j] = `　${splited[j]}`;
+			}
+		}
+		Array.prototype.push.apply(result_array, splited);
 	}
 	return result_array;
 }
 // アルファベットの時は改行ロジックを変更
-function WrapAlphabetic(arr){}
+function WrapAlphabetic(arr){
+	let result_array = [];
+	for(var i = 0; i < arr.length; i++){
+		// console.log(arr[i]);
+
+		let retus = arr[i].match(/セントエルモ/);
+		// console.log(retus);
+
+		// it:Cattedrale dei Santi Erasmo e Marciano e di Santa Maria Assunta
+		// The Rime of the Ancient Mariner
 
 
-// 文字数で折り返し
+		// Array.prototype.push.apply(result_array, arr[i]);
+		result_array.push(arr[i]);
+	}
+	return result_array;
+}
+
+// 文字数で折り返し＆句読点が先頭に来ているとき改行
 function WrapLength(arr, length){
 	let result_array = [];
 	let regexp = new RegExp(".{1," + length + "}", "g");
 	for(var i = 0; i < arr.length; i++){
 		if(arr[i].length >= length){
 			var splited = arr[i].match(regexp);
-			Array.prototype.push.apply(result_array, splited)
+			// 句読点が先頭に来ているとき改行
+			for(var j = 0; j < splited.length; j++){
+				let laststr = splited[j].slice(0, 1);
+				if(j > 0 && ["。", "、"].includes(laststr)){
+					splited[j-1] = `${splited[j-1]}${splited[j].slice(0, 1)}`;
+					splited[j] = splited[j].slice(1);
+				}
+			}
+			Array.prototype.push.apply(result_array, splited);
 		}
 	}
 	return result_array;
 }
 
 
-
 // 他タスク
-// 句読点が先頭に来ているとき改行
-// 先頭にスペースがある場合改行
-
 
 // 転び字を実装
 // 鏡文字を実装
 // トップ文字を大きくするやつを実装
 
 // フォント・文字サイズ設定を実装
-// CSS調整
 
 
 function ArticleCanvas(ctx, x, y, text){
-	if(typeof ctx != 'object' || typeof x != 'number' || typeof y  != 'number'){
+	if(typeof ctx != 'object' || typeof x != 'number' || typeof y  != 'number' || typeof text  != 'string'){
 		throw '必要な値がないか、形式が正しくありません';
 	}
 
@@ -49,13 +73,15 @@ function ArticleCanvas(ctx, x, y, text){
 	let fonttipe = 'inherit';
 
 	// 折り返し処理
-	let text_arr = [];
-	text_arr.push(text);
-
+	let text_arr = [text];
 	text_arr = WrapLF(text_arr);
+
+	text_arr = WrapAlphabetic(text_arr);
 
 	let chara_length = Math.floor(textwidth / fontsize);
 	text_arr = WrapLength(text_arr, chara_length);
+
+	console.log(text_arr);
 
 	// 出力
 	let position_x = x;
